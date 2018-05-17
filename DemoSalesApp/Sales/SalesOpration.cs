eb289497e1ec635d6e45b58c3db7fae92d1f341f
Buddhika Mayadunna <b.mayadunna@sana-commerce.com>
2018-05-17 22:35:35 +0530

    initial commit

---
+++ DemoSalesApp/Sales/SalesOpration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSalesApp.Sales
+{
+    public class SalesOpration
+    {
+
+        public double CalculateTaxIncludedTotal(double subTotal,double taxPercentage)
+        {          
+            if (subTotal > 0)
+            {
+                if(taxPercentage > 0 && taxPercentage <= 100)
+                { 
+                    return subTotal + (subTotal * (taxPercentage *  0.01));
+                }
+                else
+                {
+                    return subTotal;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+    }
+}
---
+++ DemoSalesAppTests/Sales/SalesOprationTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoSalesApp.Sales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSalesApp.Sales.Tests
+{
+    [TestClass()]
+    public class SalesOprationTests
+    {
+        [TestMethod()]
+        public void CalculateTaxIncludedTotalTest_EmptySubTotal_return0()
+        {
+            var salesOparation = new SalesOpration();
+            double expected = 112;
+            double subTotal = 100;
+            double taxPercentage = 12;
+
+            var TaxIncludedSubTotal = salesOparation.CalculateTaxIncludedTotal(subTotal, taxPercentage);
+            Assert.AreEqual(expected, TaxIncludedSubTotal);
+        }
+    }
+}
